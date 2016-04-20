425af76a809240b8bfc7e1ca3f7faf2cb9afa91f
Guillaume CAMUS <guillaume.camus@gmail.com>
2016-04-20 06:42:06 -0400

    Fix provision

--- provision/ansible/roles/motd/files/update_motd.sh
+++ provision/ansible/roles/motd/files/update_motd.sh
@@ -7,3 +7,4 @@ DATA=`df -Ph | grep data | awk '{print $4}' | tr -d '\n'`
 echo -e "\e[38;5;17m \e[38;5;39mCurrent Users \e[38;5;93m=  \e[38;5;27m$USER"
 echo -e "\e[38;5;17m\e[38;5;39mDiskSpace DATA \e[38;5;93m=  \e[38;5;27m$DATA"
 echo -e "\e[38;5;17m          \e[38;5;39mDate \e[38;5;93m=  \e[38;5;27m$(date)"
+echo -e "\e[0m"

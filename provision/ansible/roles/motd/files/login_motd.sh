dedea5368d118292460ccbc57708554701e82431
Guillaume CAMUS <guillaume.camus@gmail.com>
2016-05-22 05:31:45 -0400

    Fix motd

--- provision/ansible/roles/motd/files/login_motd.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/usr/bin/update_motd.sh
--- provision/ansible/roles/motd/files/update_motd.sh
+++ provision/ansible/roles/motd/files/update_motd.sh
@@ -2,9 +2,7 @@
 echo -e "\e[38;5;39m"
 figlet -c "Guiyomh VM"
 echo -e "\e[0m"
-/usr/bin/generate_motd.sh -t Modern
-DATA=`df -Ph | grep data | awk '{print $4}' | tr -d '\n'`
+/usr/bin/FireMotD.sh
 echo -e "\e[38;5;17m \e[38;5;39mCurrent Users \e[38;5;93m=  \e[38;5;27m$USER"
-echo -e "\e[38;5;17m\e[38;5;39mDiskSpace DATA \e[38;5;93m=  \e[38;5;27m$DATA"
 echo -e "\e[38;5;17m          \e[38;5;39mDate \e[38;5;93m=  \e[38;5;27m$(date)"
 echo -e "\e[0m"
